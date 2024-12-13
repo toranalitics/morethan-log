927f74f0c816559fb69c8df0537e7a7ba463ce97
toranalytics <tjwnsqja0704@gmail.com>
2024-12-13 16:14:48 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,27 @@ import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
 
 import "/src/styles/themes/prism.css"
+import { useEffect } from "react"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
+
+  useEffect(() => {
+    // Google Tag 코드 추가
+    const script1 = document.createElement('script');
+    script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-9P1NERCL82';
+    script1.async = true;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-9P1NERCL82');
+    `;
+    document.body.appendChild(script2);
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -19,5 +37,3 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
     </QueryClientProvider>
   )
 }
-
-export default App
